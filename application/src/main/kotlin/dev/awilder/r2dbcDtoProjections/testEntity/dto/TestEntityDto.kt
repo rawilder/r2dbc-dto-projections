b09410a10065a4c9589889e09295ae30aec3dde8
awilder <rawilder0506@gmail.com>
2022-04-29 19:10:02 -0500

    first test in TestEntityTest fails because of dto projection

--- application/src/main/kotlin/dev/awilder/r2dbcDtoProjections/testEntity/dto/TestEntityDto.kt
+++ application/src/main/kotlin/dev/awilder/r2dbcDtoProjections/testEntity/dto/TestEntityDto.kt
@@ -1,11 +1,9 @@
 package dev.awilder.r2dbcDtoProjections.testEntity.dto
 
-import io.micronaut.core.annotation.Introspected
 import io.micronaut.serde.annotation.Serdeable
 import java.util.UUID
 
 @Serdeable
-@Introspected
 data class TestEntityDto (
     val id: UUID,
     val name: String
